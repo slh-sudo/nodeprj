627fcf7db72aed68f320a36e8079b1b26bb4b1fe
slh-sudo <rtensho@gmail.com>
2025-02-11 15:59:33 +0900

    second.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Welcome to my web server!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
